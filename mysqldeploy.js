9b56807af98f5a5c9689030cb88fb597a33aca3a
Sachin Hridayraj <sraj@microsoft.com>
2016-02-09 15:09:52 -0800

    added mysqldeploy

--- mysqldeploy.js
+++ mysqldeploy.js
@@ -4,20 +4,55 @@ var tl = require('vso-task-lib');
 var echo = new tl.ToolRunner(tl.which('echo', true));
 
 var msg = tl.getInput('msg', true);
-echo.arg(msg);
+var dbname = tl.getInput('dbname', true);
+var username = tl.getInput('username', true);
+var password = tl.getInput('password', true);
+var createdb = tl.getBoolInput('createdb', true);
+var scriptfile = tl.getPathInput('scriptfile', false);
 
-var cwd = tl.getPathInput('cwd', false);
+dbname = dbname.trim();
+username = username.trim();
+password = password.trim();
 
-// will error and fail task if it doesn't exist
-tl.checkPath(cwd, 'cwd');
-tl.cd(cwd);
+// Define error handler
+var onError = function (errorMsg) {
+    tl.error(errorMsg);
+    tl.exit(1);
+   // echo (errorMsg);
+}
+var mysqlshell = tl.createToolRunner(tl.which('bash', true));
 
-echo.exec({ failOnStdErr: false})
-.then(function(code) {
-    tl.exit(code);
+if (createdb){
+        var mysqlcmd = 'mysql -u '+username+' --password='+password+' -e "create database '+dbname+'"';
+        mysqlshell.arg(mysqlcmd);
+mysqlcmd.exec()
+.then(function (code) {
+    // Executed successfully
+    tl.setResult(tl.TaskResult.Succeeded, tl.loc('BashReturnCode', code));
 })
-.fail(function(err) {
-    console.error(err.message);
-    tl.debug('taskRunner fail');
+.fail(function (err) {
+    // Error executing
+    tl.debug('ToolRunner execution failure: ' + err);
+    tl.debug('Error creatinng db with command '+mysqlcmd);
     tl.exit(1);
 })
+}
+
+var mysqlcmd = 'mysql -u '+username+' --password='+password+' -D '+dbname+' <'+scriptfile;
+mysqlshell.arg(mysqlcmd);
+mysqlcmd.exec()
+.then(function (code) {
+    // Executed successfully
+    tl.setResult(tl.TaskResult.Succeeded, tl.loc('BashReturnCode', code));
+})
+.fail(function (err) {
+    // Error executing
+    tl.debug('ToolRunner execution failure: ' + err);
+    tl.debug('Error Executing '+mysqlcmd);
+    tl.exit(1);
+})
+
+}
+
+
+
